9a220af4f1a95684dd90cf0a488609b7b8805ed6
bramhanandkaleJosh <bramhanand.kale@joshsoftware.com>
2023-01-27 14:24:27 +0530

    other test and debuging code

--- amazon-kinesis-video-streams-producer-sdk-java-master/src/main/ConfigCustomization/utils/WebcamUtils.java
+++ amazon-kinesis-video-streams-producer-sdk-java-master/src/main/ConfigCustomization/utils/WebcamUtils.java
@@ -1,20 +1,27 @@
 package utils;
 
 import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamResolution;
 import com.github.sarxos.webcam.util.ImageUtils;
+import org.jcodec.api.transcode.Transcoder;
+import org.jcodec.common.model.ColorSpace;
+import org.jcodec.common.model.Picture;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class WebcamUtils {
 
+    public static void main(String[] args) {
+        webcamStreaming();
+    }
+
     public static byte[] getImageByteArrayFromWebCam(Webcam webcam){
         webcam.open();
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         try {
-            ImageIO.write(webcam.getImage(), ImageUtils.FORMAT_JPG, baos);
+            ImageIO.write(webcam.getImage(), ImageUtils.FORMAT_WBMP, baos);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -24,12 +31,31 @@ public class WebcamUtils {
     }
 
     public static void webcamStreaming(){
-        Webcam.setAutoOpenMode(true);
-        Webcam webcam = Webcam.getDefault();
-        Dimension dimension = new Dimension(320, 240);
-        webcam.setViewSize(dimension);
+        try {
 
-//        StreamServerAgent serverAgent = new StreamServerAgent(webcam, dimension);
-//        serverAgent.start(new InetSocketAddress("localhost", 0));
+            Webcam.setAutoOpenMode(true);
+            Webcam webcam = Webcam.getDefault();
+//            Dimension dimension = new Dimension(WebcamResolution.VGA.getWidth(), WebcamResolution.VGA.getHeight());
+//            webcam.setViewSize(dimension);
+//        webcam.getImageBytes()
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            ImageIO.write(webcam.getImage(), ImageUtils.FORMAT_JPG, outputStream);
+
+
+            Picture picture = Picture.create(WebcamResolution.VGA.getWidth(), WebcamResolution.VGA.getHeight(), ColorSpace.YUV420);
+//            H264Encoder encoder = new H264Encoder(new DumbRateControl());
+//            ByteBuffer byteBuffer = encoder.encodePFrame(picture, ByteBuffer.wrap(outputStream.toByteArray()));
+            outputStream.close();
+
+
+           Transcoder trsn = Transcoder.newTranscoder().create();
+//           trsn.
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
